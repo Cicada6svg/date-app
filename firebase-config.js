5520101be3571e43b1d87ed316e8110af446c628
Cicada6svg <barakaochieng93@gmail.com>
2025-11-20 20:38:56 +0300

    Add Firebase configuration file

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBjBfv_OAxXzY8ITG5JM4DLNr_S61bD61k",
+  authDomain: "com.cicatrixintl.campuscrush254",
+  projectId: "campus-crush-b",
+  storageBucket: "campus-crush-b.firebasestorage.app",
+  messagingSenderId: "862850121932",
+  appId: "1:862850121932:android:5ec93ff1d68ed32e988269"
+};
