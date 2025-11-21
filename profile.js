e049425f6e76654d1d3a4563dfd36796c654c0e8
Cicada6svg <barakaochieng93@gmail.com>
2025-11-21 07:44:20 +0300

    Update Firebase configuration in profile.js
    Updated firebaseConfig with new API key and identifiers.

--- profile.js
+++ profile.js
@@ -12,7 +12,14 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 // Replace with your config or import shared firebase-config.js
-const firebaseConfig = { 
+const firebaseConfig = {
+   apiKey: "AIzaSyC830lhq-2I0lBMxdT-SPFJVNkVtK96z38",
+  authDomain: "campus-crush-b.firebaseapp.com",
+  projectId: "campus-crush-b",
+  storageBucket: "campus-crush-b.firebasestorage.app",
+  messagingSenderId: "862850121932",
+  appId: "1:862850121932:web:7b8d75688564d02f988269",
+  measurementId: "G-M6K5200C05" 
 
 };
 
